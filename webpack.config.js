f67443c2aed9976c6b2f08e8fa738f5b2701e688
Andrii <youdronveduta@gmail.com>
2024-08-05 14:52:53 +0200

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,13 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const isProduction = process.env.NODE_ENV == 'production';
-
+const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve('dist'),
+        clean: true,
     },
     devServer: {
         open: true,
@@ -17,36 +15,37 @@ const config = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'index.html',
+            template: './index.html', 
         }),
-
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/i,
-                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
             },
             {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
+                test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: 'url-loader?limit=10000',
             },
-
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
+            {
+                test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+                use: 'file-loader',
+            },
         ],
     },
+    mode: isProduction ? 'production' : 'development'
 };
 
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
-};
+export default config;
