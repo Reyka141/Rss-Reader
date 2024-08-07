ba2a62886dd6893b03ba8e82b8f60065745864dc
Andrii <youdronveduta@gmail.com>
2024-08-07 14:29:57 +0200

    update src/app.js

--- src/app.js
+++ src/app.js
@@ -1,21 +1,52 @@
 import * as yup from 'yup';
 import i18next from 'i18next';
 import axios from 'axios';
+import uniqueId from 'lodash/uniqueId.js';
 import watch from './view.js';
 import resources from './locales/index.js';
+
+const routes = {
+  rssPath: (url) => `https://allorigins.hexlet.app/get?disableCache=true&url=${url}`,
+};
+
+const parserFn = (response) => {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(response.data.contents, 'application/xml');
+  const items = [...doc.querySelectorAll('item')].map((item) => {
+    const id = uniqueId();
+    const title = item.querySelector('title').textContent;
+    const description = item.querySelector('description').textContent;
+    const link = item.querySelector('link').textContent;
+    return {
+      id,
+      title,
+      description,
+      link,
+    };
+  });
+  return {
+    title: doc.querySelector('title').textContent,
+    description: doc.querySelector('description').textContent,
+    items,
+  };
+};
 
 export default () => {
   const elements = {
     form: document.querySelector('.rss-form'),
     input: document.querySelector('#url-input'),
     feedback: document.querySelector('.feedback'),
+    button: document.querySelector('[type=submit]'),
   };
+
   const defaultLang = 'ru';
+
   const state = {
-    status: 'filling',
+    status: 'filling', // 'sending', 'sent', 'error'
     valid: false,
     errors: [],
     loadedFeeds: [],
+    contents: {},
   };
 
   const i18n = i18next.createInstance();
@@ -47,9 +78,13 @@ export default () => {
       const newRss = Object.fromEntries(formData);
       console.log(newRss.url);
       schema.validate(newRss, { abortEarly: false })
-        .then(() => axios.get(`/get?disableCache=true&url=${newRss.url}`))
+        .then(() => {
+          watchedState.status = 'sending';
+          return axios.get(routes.rssPath(newRss.url));
+        })
         .then((response) => {
-          console.log(response);
+          watchedState.contents = parserFn(response);
+          watchedState.status = 'filling';
           watchedState.errors = [];
           watchedState.loadedFeeds.push(newRss.url);
           watchedState.valid = true;
